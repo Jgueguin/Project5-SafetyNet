bf3bfd733e3b9d83e863a3b83a6d4adcc6060298
Jérôme <jerome.gueguin@ik.me>
2021-08-04 15:37:49 +0200

    MAJ 2021-08-04

--- src/main/java/com/safetynet/App.java
+++ src/main/java/com/safetynet/App.java
@@ -1,19 +1,7 @@
 package com.safetynet;
 
-import com.fasterxml.jackson.databind.ObjectMapper;
-import com.safetynet.repository.PersonsRepository;
-import com.safetynet.service.PersonsService;
-import jdk.internal.org.objectweb.asm.TypeReference;
-import org.springframework.boot.CommandLineRunner;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
-import org.springframework.context.annotation.Bean;
-
-import java.io.IOException;
-import java.io.InputStream;
-import java.util.List;
-
-import com.safetynet.model.*;
 
 @SpringBootApplication
 public class App {
@@ -28,7 +16,7 @@ public class App {
 	public void run(String... args) throws Exception {
 		System.out.println("Hello World, hey, hey!");
 	}*/
-
+/*
 	@Bean
 	CommandLineRunner runner(PersonsService personService) {
 		return args -> {
@@ -38,21 +26,24 @@ public class App {
 
 			// TypeReference<List<User>> typeReference = new TypeReference<List<User>>(){};
 
-			TypeReference<List<Persons>> typeReference = new TypeReference<List<Persons>>(){};
+			// ?????????
+			TypeReference<Persons> typeReference = new TypeReference<List<Persons>>(){};
+			// ?????????
 
 			InputStream inputStream = TypeReference.class.getResourceAsStream("Data.json");
 
-
 			try {
-				List<PersonsRepository> persons = mapper.readValue(inputStream,typeReference);
-				personService.savePersons(persons);
+				List<Persons> persons = mapper.readValue(inputStream,typeReference);
+
+				personService.savePersons((Persons) persons);
+
 				System.out.println("Users Saved!");
 			} catch (IOException e){
 				System.out.println("Unable to save persons: " + e.getMessage());
 			}
-		};
+		};*/
 	}
 
 
 
-}
+
